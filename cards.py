b5ab54de0390f9157d92e01f9c092d7a8dd6b2e3
Jack <the.jack.turner@gmail.com>
2019-01-04 14:41:23 +0000

    Got the minion classes working

--- cards.py
+++ cards.py
@@ -1,22 +1,26 @@
 class Card(object):
-	def __init__(self, name):
+	def __init__(self, name, description):
 		self.name = name
-
-	def __repr__(self):
-		return self.name
+		self.description = description
 
 class Minion(Card):
-	def __init__(self, name, attack, health, description):
-		self.name = name
+	def __init__(self, name, description, attack, health):
 		self.attack = attack
 		self.health = health
-		self.description = description
-		self.summary = self.name + " , Att: " + str(self.attack) + " , HP: " + str(self.health)
+		super(Minion, self).__init__(name, description)
 
 	def __repr__(self):
-		return " Name - %s \n Attack - %d \n Health - %d \n %s" % (self.name, self.attack, self.health, self.description)
+		return "%s \nAtt:-%d  HP:-%d\n%s" % (self.name, self.attack, self.health, self.description)
 
 
-brute_minion = Minion("Brute", 1, 4, "Not as strong as he thinks he is.")
+class Brute(Minion):
+	def __init__(self):
+		super(Brute, self).__init__("Brute", "Not as strong as he thinks he is.", 1, 4)
+
+
 raptor_man_minion = Minion("Raptor Man", 2, 2, "Really more of a raptor boy.")
 widowmaker_minion = Minion("Widowmaker", 3, 1, "Hope you don't find out why they call her that.")
+
+
+print Brute()
+
--- game.py
+++ game.py
@@ -26,3 +26,4 @@ def play():
 		sleep(5)
 		
 play()
+
